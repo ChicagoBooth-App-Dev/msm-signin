963b4aff97ae9d854dac8d61c3ec819d74556b3b
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2020-11-17 03:19:08 +0000

    attempt to rollback database and input movie count

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -10,9 +10,9 @@
 #
 class Bookmark < ApplicationRecord
 
-  belongs_to(:movie, { :required => false, :class_name => "Movie", :foreign_key => "movie_id" })
+  belongs_to(:movie, { :required => false, :class_name => "Movie", :foreign_key => "movie_id"})
 
-  belongs_to(:user, { :required => false, :class_name => "User", :foreign_key => "user_id" })
+  belongs_to(:user, { :required => false, :class_name => "User", :foreign_key => "user_id"})
 
 
 end
